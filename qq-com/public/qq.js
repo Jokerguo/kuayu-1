66688851fc2b6e76c1ac0cffff5786d4a10e971a
jokerguo <1977036313@qq.com>
2020-01-14 16:10:52 +0800

    kuayu

---
+++ qq-com/public/qq.js
@@ -0,0 +1,11 @@
+const request = new XMLHttpRequest()
+request.open('GET','./friends.json')
+request.onreadystatechange =()=>{
+    if(request.readyState === 4){
+        if(request.status >=200 && request.status <400){
+            console.log(request.response)
+        }
+        
+    }
+}
+request.send()
